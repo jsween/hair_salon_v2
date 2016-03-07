bc9b1c88c01c36c23b3f0752981e2dcc22bfd04e
Jon <jonsweeney26@gmail.com>
2016-03-06 19:42:33 -0800

    rename the databases to proper names

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -6,7 +6,7 @@
     require_once __DIR__.'/../src/Stylist.php';
     require_once __DIR__.'/../src/Client.php';
 
-    $server = 'mysql:host=localhost:8889;dbname=hair_salon_db_test';
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon_test';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
