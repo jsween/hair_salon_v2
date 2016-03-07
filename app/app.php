909f30508800ec34eaccc4a0fbb008269c5b9dab
Jon <jonsweeney26@gmail.com>
2016-03-06 16:11:49 -0800

    init commit

---
+++ app/app.php
@@ -0,0 +1,20 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
+
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon_db';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    return $app;
+ ?>
